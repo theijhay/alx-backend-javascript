8b92a9f5e4c2d0b9c58b6ce314eda41c2af7e9ff
theijhay <olawaleisaacjohn@gmail.com>
2024-04-22 18:11:05 +0100

    modifying using constants and let methods

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,12 @@
 export default function taskBlock(trueOrFalse) {
   /* eslint-disable no-unused-vars */
   /* eslint-disable no-var */
-  let task = false;
-  let task2 = true;
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    const task = true;
+    const task2 = false;
   }
-
   return [task, task2];
 }
