de3471941c36b79894b93b5ae9c3a844bad21a5b
theijhay <olawaleisaacjohn@gmail.com>
2024-04-22 18:40:58 +0100

    modifying using constants and let methods

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  /* eslint-disable no-unused-vars */
+  /* eslint-disable no-var */
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
-    // We'll use a different variable name inside the conditional block
-    const newTask = true;
-    const newTask2 = false;
-    task = newTask;
-    task2 = newTask2;
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
