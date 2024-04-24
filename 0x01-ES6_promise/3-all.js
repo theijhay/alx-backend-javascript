177df2c20bae63c78dcdb4ea0e414d9ce773bb54
theijhay <olawaleisaacjohn@gmail.com>
2024-04-24 10:36:55 +0100

    import uploadPhoto and createUser from utils.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,12 @@
-/* eslint-disable import/extensions */
 import { uploadPhoto, createUser } from './utils';
 
 export default async function handleProfileSignup() {
   try {
-    const [photoResponse, userResponse] = await Promise.all([uploadPhoto(), createUser()]);
-    const photo = photoResponse.body;
-    const { firstName, lastName } = userResponse;
+    const photoResponse = await uploadPhoto();
+    const userResponse = await createUser();
 
-    console.log(`${photo} ${firstName} ${lastName}`);
+    console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
   } catch (error) {
-    console.error('Signup system offline');
+    console.log('Signup system offline');
   }
 }
