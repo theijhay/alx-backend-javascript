fe475a09886160322cb516fd2b4c8a2afae04270
theijhay <olawaleisaacjohn@gmail.com>
2024-04-24 12:32:53 +0100

    A function named guardrail that will accept one argument mathFunction (Function)

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,11 +1,16 @@
 export default function guardrail(mathFunction) {
   const queue = [];
-  try {
-    queue.push(mathFunction());
-  } catch (error) {
-    queue.push(error.message);
-  } finally {
+
+  const processResult = (result) => {
+    queue.push(result);
     queue.push('Guardrail was processed');
+    return queue;
+  };
+
+  try {
+    const res = mathFunction();
+    return processResult(res);
+  } catch (err) {
+    return processResult(err.toString());
   }
-  return queue;
 }
