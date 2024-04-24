2de2abe597f34c5fe3d84d7705e3ef95f0293071
theijhay <olawaleisaacjohn@gmail.com>
2024-04-24 12:40:00 +0100

    Import uploadPhoto and createUser from utils.js

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+const asyncUploadUser = async () => {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return { photo, user };
+  } catch (error) {
+    return { photo: null, user: null };
+  }
+};
+
+export default asyncUploadUser;
