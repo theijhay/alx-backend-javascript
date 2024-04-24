88da9d0dc0b72d8ab7dc97ae12061f1b1c24b162
theijhay <olawaleisaacjohn@gmail.com>
2024-04-24 08:01:45 +0100

    Using the function prototype handleResponseFromAPI(promise)

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,11 @@
-export default async function handleResponseFromAPI(promise) {
-  try {
-    const response = await promise;
-    console.log('Got a response from the API');
-    return { status: 200, body: 'success', response };
-  } catch (error) {
-    console.error(error); // Log the error for debugging
-    return new Error('The fake API is not working currently');
-  }
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then((response) => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success', response };
+    })
+    .catch((error) => {
+      console.error(error); // Log the error for debugging
+      return Promise.reject(new Error('The fake API is not working currently'));
+    });
 }
