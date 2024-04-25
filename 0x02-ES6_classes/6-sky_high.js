c974b203bc0d8178798a09dea1d1e53573717d5e
theijhay <olawaleisaacjohn@gmail.com>
2024-04-25 19:47:34 +0100

    Implement a class named SkyHighBuilding that extends from Building

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,21 +1,20 @@
-// 6-sky_high.js
 import Building from './5-building.js';
 
-class SkyHighBuilding extends Building {
+export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-    super(sqft); // Call the constructor of the parent class and pass the sqft parameter
-    this._floors = floors; // Initialize the _floors attribute with the value of floors passed to the constructor
+    super(sqft);
+    this._floors = floors;
   }
 
-  // Getter for floors attribute
+  get sqft() {
+    return this._sqft;
+  }
+
   get floors() {
     return this._floors;
   }
 
-  // Override the evacuationWarningMessage method
   evacuationWarningMessage() {
     return `Evacuate slowly the ${this._floors} floors`;
   }
 }
-
-export default SkyHighBuilding;
