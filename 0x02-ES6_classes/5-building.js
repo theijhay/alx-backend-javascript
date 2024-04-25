0b67f6f44e1af62cf196e96cf88f17968acab94f
theijhay <olawaleisaacjohn@gmail.com>
2024-04-25 19:34:22 +0100

    Implement a class named Building.

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,27 +1,16 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
-
-    if (
-      this.constructor !== Building
-        && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage
-    ) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
   }
 
-  // Getter for sqft
   get sqft() {
     return this._sqft;
   }
 
-  // Setter for sqft
   set sqft(value) {
     this._sqft = value;
-  }
-
-  // Default implementation of evacuationWarningMessage
-  evacuationWarningMessage() {
-    throw new Error('evacuationWarningMessage must be implemented by subclasses');
   }
 }
