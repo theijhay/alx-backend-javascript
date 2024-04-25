870525e44416846935f37156631e785e317e89be
theijhay <olawaleisaacjohn@gmail.com>
2024-04-25 18:30:38 +0100

    Implementing a class named Pricing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -2,53 +2,52 @@
 import Currency from './3-currency.js';
 
 class Pricing {
-    constructor(amount, currency) {
-        this._amount = 0;
-        this._currency = null;
+  constructor(amount, currency) {
+    this._amount = 0;
+    this._currency = null;
 
-        this.amount = amount; // Using the setter to validate and set the amount
-        this.currency = currency; // Using the setter to validate and set the currency
-    }
+    this.amount = amount; // Using the setter to validate and set the amount
+    this.currency = currency; // Using the setter to validate and set the currency
+  }
 
-    // Getter and setter for amount
-    get amount() {
-        return this._amount;
-    }
+  // Getter and setter for amount
+  get amount() {
+    return this._amount;
+  }
 
-    set amount(value) {
-        if (typeof value === 'number') {
-            this._amount = value;
-        } else {
-            throw new TypeError("Amount must be a number");
-        }
+  set amount(value) {
+    if (typeof value === 'number') {
+      this._amount = value;
+    } else {
+      throw new TypeError('Amount must be a number');
     }
+  }
 
-    // Getter and setter for currency
-    get currency() {
-        return this._currency;
-    }
+  // Getter and setter for currency
+  get currency() {
+    return this._currency;
+  }
 
-    set currency(value) {
-        if (value instanceof Currency) {
-            this._currency = value;
-        } else {
-            throw new TypeError("Currency must be an instance of Currency class");
-        }
+  set currency(value) {
+    if (value instanceof Currency) {
+      this._currency = value;
+    } else {
+      throw new TypeError('Currency must be an instance of Currency class');
     }
+  }
 
-    // Method to display full price format
-    displayFullPrice() {
-        return `${this._amount} ${this._currency.name} (${this._currency.code})`;
-    }
+  // Method to display full price format
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
 
-    // Static method to convert price based on conversion rate
-    static convertPrice(amount, conversionRate) {
-        if (typeof amount === 'number' && typeof conversionRate === 'number') {
-            return amount * conversionRate;
-        } else {
-            throw new TypeError("Amount and conversion rate must be numbers");
-        }
+  // Static method to convert price based on conversion rate
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount === 'number' && typeof conversionRate === 'number') {
+      return amount * conversionRate;
     }
+    throw new TypeError('Amount and conversion rate must be numbers');
+  }
 }
 
 export default Pricing;
