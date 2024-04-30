7c03a8a19f0edea782255238c654b71c08311f4f
theijhay <olawaleisaacjohn@gmail.com>
2024-04-30 13:20:36 +0100

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,11 @@
-function cleanSet(set, startString) {
-  const cleanedValues = [];
-
-  for (const value of set) {
-    if (value.startsWith(startString)) {
-      cleanedValues.push(value.slice(startString.length));
+// eslint-disable-next-line consistent-return
+export default function cleanSet(set, startString) {
+  let res = '';
+  if (!startString || !startString.length) return res;
+  for (const el of set) {
+    if (el && el.startsWith(startString)) {
+      res += `${el.slice(startString.length)}-`;
     }
   }
-
-  return cleanedValues.join('-');
+  return res.slice(0, res.length - 1);
 }
-
-export default cleanSet;
