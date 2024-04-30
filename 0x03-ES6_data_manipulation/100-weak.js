df24d67517cc6eee33389b412f0c342b6c58b379
theijhay <olawaleisaacjohn@gmail.com>
2024-04-30 14:19:40 +0100

    Weak link data structure

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,13 @@
+const weakMap = new WeakMap();
+
+function queryAPI(endpoint) {
+  let count = weakMap.get(endpoint) || 0;
+  count++;
+  weakMap.set(endpoint, count);
+
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+}
+
+export { weakMap, queryAPI };
