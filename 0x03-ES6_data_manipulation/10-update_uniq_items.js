506ef1664aa62225affbf92b68870ed5c2c18444
theijhay <olawaleisaacjohn@gmail.com>
2024-04-30 14:10:48 +0100

    More map data structure

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,15 @@
+function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  for (const [item, quantity] of map.entries()) {
+    if (quantity === 1) {
+      map.set(item, 100);
+    }
+  }
+
+  return map;
+}
+
+export default updateUniqueItems;
