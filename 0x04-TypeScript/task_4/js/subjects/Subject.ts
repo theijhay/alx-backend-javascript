1d1ca1632b2b4099dd6cba96e449a07ac29360ea
theijhay <olawaleisaacjohn@gmail.com>
2024-05-01 20:32:55 +0100

    Namespace & Declaration merging

---
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,11 @@
+/// <reference path="./Teacher.ts" />
+
+namespace Subjects {
+    export class Subject {
+      protected teacher: Teacher;
+  
+      setTeacher(teacher: Teacher): void {
+        this.teacher = teacher;
+      }
+    }
+  }
