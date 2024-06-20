d3dc393c5f8092e4d73b4d604bf80efad60ff8b6
theijhay <olawaleisaacjohn@gmail.com>
2024-06-20 17:20:37 +0100

    Integration testing

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,26 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('./api');
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+describe('Index page', () => {
+    it('should return status code 200', (done) => {
+        chai.request(app)
+            .get('/')
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                done();
+            });
+    });
+
+    it('should return the correct message', (done) => {
+        chai.request(app)
+            .get('/')
+            .end((err, res) => {
+                expect(res.text).to.equal('Welcome to the payment system');
+                done();
+            });
+    });
+});
